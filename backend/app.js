82d6a86e797e3732b9d30a5b9117e9e53a94429f
Anastasiia Kunstman <anastasiia.kunstman@yandex.ru>
2023-07-09 22:59:24 +0500

    cors

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,9 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://akunstman.nomoreparties.sbs',
+}));
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
