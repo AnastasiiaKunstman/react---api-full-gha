e143ae0d2802ccd89f2b2ac31802e64f3dcaea87
Anastasiia Kunstman <anastasiia.kunstman@yandex.ru>
2023-07-09 23:59:40 +0500

    ПР15

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect('mongodb://0.0.0.0/mestodb');
 
 app.use(requestLogger);
 
