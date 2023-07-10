51b955abf76de2f6f1bc0173285092833018cd77
Anastasiia Kunstman <anastasiia.kunstman@yandex.ru>
2023-07-10 07:18:17 +0500

    ревью1

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,12 @@
 const {
   NODE_ENV,
   JWT_SECRET,
-  MONGODB_URI,
+  MONGO_URL,
   PORT,
 } = process.env;
 
 const config = {
-  MONGODB_URI: NODE_ENV === 'production' ? MONGODB_URI : 'mongodb://localhost:27017/mestodb',
+  MONGO_URL: NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/mestodb',
   PORT: NODE_ENV === 'production' ? PORT : 3000,
   JWT_SECRET: NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
 };
