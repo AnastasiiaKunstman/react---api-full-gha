a7dadfdaf93e9b74bf9fd64800a903d677ebfeb0
Anastasiia Kunstman <anastasiia.kunstman@yandex.ru>
2023-07-10 08:36:05 +0500

    ПР15

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -10,7 +10,7 @@ const auth = (req, res, next) => {
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');;
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new UnauthorizedError('Неправильные почта или пароль'));
   }
