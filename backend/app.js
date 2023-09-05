1bc0b18093706f02a091c6a88afe22c41d374e13
Anastasiia Kunstman <anastasiia.kunstman@yandex.ru>
2023-09-05 16:39:38 +0500

    v1

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const routes = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(cors());
